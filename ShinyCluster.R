acc8147235aa6f85deaeda19ced5202c68675d42
Pablo Andrés Pérez <pablitoperezq.colombia@gmail.com>
2015-11-23 17:59:22 -0500

    correcciones

--- ShinyCluster.R
+++ ShinyCluster.R
@@ -1,41 +1,74 @@
-#' @import shiny methods
-#' @export
-hc_cluster <- function(names, datasets) {
+library(shiny)
+library(proxy)
+library(cluster)
+
+hierarchicalCluster <- function(m_norm, distM, clustM){
+  ### this is going to take 4-ever (O(n^2))
+  distances <- dist(t(m_norm), method=distM) ##warning: t() is the transpose function, we look for clustering words not documents
+  hclust(d=distances, method=clustM)
+}
+
+getCounts <- function(df.terms){
+  df.terms.freqs <- colSums(df.terms)
+  df.terms <- df.terms[,names(df.terms.freqs[df.terms.freqs>3])]
+  m <- as.matrix(df.terms)
+  rownames(m) <- 1:nrow(m)
+  
+  ### don't forget to normalize the vectors so Euclidean makes sense
+  norm_eucl <- function(m) m/apply(m, MARGIN=1, FUN=function(x) sum(x^2)^.5)
+  norm_eucl(m)
+}
+
+hc_cluster <- function(datasets) {
   
   shinyApp(
     
     ui = fluidPage(responsive = FALSE,
-                   fluidRow(style = "padding-bottom: 20px;",
-                            column(4, selectInput('xcol', 'Data set', names)),
-                            column(4,sliderInput("slider1", label = h3("Sliders"),
-                                                 min = 0, max = 100, value = 50))
-                            column(4, sliderInput('ycol', 'Number of clusters', dataset[nam],
-                                                  selected=names(dataset)[[2]])),
-                            column(4, numericInput('clusters', 'Cluster count', 3,
-                                                   min = 1, max = 9))
-                   ),
-                   fluidRow(
-                     plotOutput('kmeans', height = "400px")
-                   )
+            fluidRow(style = "padding-bottom: 20px;",
+                   column(4, selectInput('dataset', 'Data set', names(datasets)),
+                   column(4, selectInput('distM', 'Distance method', c("cosine","euclidean"))),
+                   column(4, selectInput('clustM', 'Clustering method', c("average", "ward.D2")))
+            ),
+            fluidRow(style = "padding-bottom: 20px;",
+                     column(4, sliderInput('k', 'Number of clusters',
+                                           min = 2, max = 10, value = 5)),
+                     column(4, sliderInput("minFreqTerms", label = h3("Min. frequency of terms"),
+                                           min = 1, max = 100, value = 3)),
+                     column(4, numericInput('clusters', 'Cluster count', 3,
+                                            min = 1, max = 9))
+            ),
+            fluidRow(
+              plotOutput('tree', height = "400px")
+            )
+         )
     ),
     
     server = function(input, output, session) {
       
+      reactive({
+        summary(datasets[input$dataset])
+      }
+      )
+      
       # Combine the selected variables into a new data frame
       selectedData <- reactive({
-        dataset[, c(input$xcol, input$ycol)]
+        datasets[input$dataset]
       })
       
       clusters <- reactive({
-        kmeans(selectedData(), input$clusters)
+        hierarchicalCluster(getCounts(selectedData()), input$distM, input$clustM)
       })
       
-      output$kmeans <- renderPlot(height = 400, {
+      output$tree <- renderPlot(height = 400, {
+        hc <- clusters()
         par(mar = c(5.1, 4.1, 0, 1))
-        plot(selectedData(),
-             col = clusters()$cluster,
-             pch = 20, cex = 3)
-        points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+        plot(hc, hang=-1, main = input$dataset)
+        groups <- cutree(hc, k=input$clusters)   # "k=" defines the number of clusters you are using
+        rect.hclust(hc, k=input$clusters, border="red") # draw dendogram with red borders around the 5 clusters   
+        
+        #cl <- cutree(hc, 30)
+        #table(cl)
+        
       })
     },
     
